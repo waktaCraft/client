76f058c151e2f957d48c4e804e31a46419ed44d0
CGaren <cgarenchan@gmail.com>
2023-02-27 20:41:29 +0900

    initial settings

--- public/electron.js
+++ public/electron.js
@@ -1,11 +1,14 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+
 app.whenReady().then(() => {
-  const win = new BrowserWindow()
+  const win = new BrowserWindow({
+    fullscreen: true,
+  })
+  win.removeMenu();
   if (process.env.NODE_ENV === 'development') {
     console.log("dev");
     win.loadURL('http://localhost:3000')
   } else {
-    console.log("prod");
     win.loadFile(
       `${path.join(__dirname, '../build/index.html')}`
     )
--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+  const canvas = document.getElementById('canvas');
+  canvas.requestPointerLock();
+  canvas.addEventListener('click', (e) => {
+    canvas.requestPointerLock();
+  });
+  },[]);
+
   return (
     <div className="App">
-      <div>test</div>
+      test
+      <canvas id="canvas" width="500" height="500" style={{border:"solid 1px black"}}></canvas>
     </div>
   );
 }
